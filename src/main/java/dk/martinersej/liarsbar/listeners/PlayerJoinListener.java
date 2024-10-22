401c3c20cf0de98c5c760d47a982af393f1acfb7
Martin <martinpugdal@live.dk>
2024-10-23 00:46:42 +0200

    refactor: ressourcepack listener

--- src/main/java/dk/martinersej/liarsbar/listeners/PlayerJoinListener.java
+++ src/main/java/dk/martinersej/liarsbar/listeners/PlayerJoinListener.java
@@ -1,8 +1,5 @@
 package dk.martinersej.liarsbar.listeners;
 
-import dk.martinersej.liarsbar.LiarsBar;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -12,24 +9,16 @@ public class PlayerJoinListener implements Listener {
 
     private final String RESOURCE_PACK_URL = "https://drive.google.com/uc?export=download&id=1MyAYR_DOlh_v-8WBrxF8bgqp1hoUJ9vw";
 
-    private void setResourcePack(Player player) {
-        Bukkit.getScheduler().runTaskLater(
-            LiarsBar.get(),
-            () -> player.setResourcePack(RESOURCE_PACK_URL), 20L
-        );
-    }
-
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-        player.sendMessage("§eVi anbefaler at du bruger vores texture pack for at få den bedste oplevelse!");
-        setResourcePack(player);
+        event.getPlayer().sendMessage("§eVi anbefaler at du bruger vores texture pack for at få den bedste oplevelse!");
+        event.getPlayer().setResourcePack(RESOURCE_PACK_URL);
     }
 
     @EventHandler
     public void onResourcepackStatusEvent(PlayerResourcePackStatusEvent event) {
         if (event.getStatus() != PlayerResourcePackStatusEvent.Status.ACCEPTED) {
-            setResourcePack(event.getPlayer());
+            event.getPlayer().setResourcePack(RESOURCE_PACK_URL);
         }
     }
 }
