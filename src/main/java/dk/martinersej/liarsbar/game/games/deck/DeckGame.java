cafb1d8eddc1bba6bc1d42a1e538af518dc2aa49
Martin <martinpugdal@live.dk>
2024-10-22 14:31:37 +0200

    nms lib added & added hide card feature

--- src/main/java/dk/martinersej/liarsbar/game/games/deck/DeckGame.java
+++ src/main/java/dk/martinersej/liarsbar/game/games/deck/DeckGame.java
@@ -1,8 +1,14 @@
 package dk.martinersej.liarsbar.game.games.deck;
 
 import dk.martinersej.liarsbar.game.Game;
+import dk.martinersej.liarsbar.game.GamePlayer;
 import dk.martinersej.liarsbar.game.GameType;
 import dk.martinersej.liarsbar.game.games.deck.deck.Deck;
+import dk.martinersej.liarsbar.game.games.deck.deck.DeckItem;
+import net.minecraft.server.v1_8_R3.PacketPlayOutEntityEquipment;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftItemStack;
+import org.bukkit.entity.Player;
 
 public class DeckGame extends Game {
 
@@ -29,4 +35,14 @@ public class DeckGame extends Game {
 
     }
 
+    public void visualizeHiddenCards(DeckPlayer deckPlayer) {
+        PacketPlayOutEntityEquipment packet = new PacketPlayOutEntityEquipment(deckPlayer.getPlayer().getEntityId(), 0, CraftItemStack.asNMSCopy(DeckItem.BLANK.getItemStack()));
+        for (GamePlayer gameplayer : getPlayers()) {
+            if (gameplayer.equals(deckPlayer)) {
+                continue;
+            }
+            Player p = deckPlayer.getPlayer();
+            ((CraftPlayer) p).getHandle().playerConnection.sendPacket(packet);
+        }
+    }
 }
